d2815fcfae154e4c2eb3873123501b43a27b243e
Nehad <35563834+81joker@users.noreply.github.com>
2025-08-08 11:47:33 +0200

    test tje home route is accessible

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,20 +6,12 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_hello_route_is_accessible(): void
-    {
-        $response = $this->get('/hello');
-        $response->assertStatus(200);
-        $response->assertSee('Hello, World!');
-    }
+
 
     public function test_home_route_is_accessible(): void 
     {
         $response = $this->get('/');
         $response->assertStatus(200);
-        $response->assertSee('Welcome to Laravel');
+        $response->assertSee('Laravel has an incredibly rich ecosystem.');
     }
 }
